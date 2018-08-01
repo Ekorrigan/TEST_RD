ab7e2ecfc58624108f64ea62dc86145578ef35a4
Ekorrigan <41920003+Ekorrigan@users.noreply.github.com>
2018-08-01 16:43:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // register service worker
 
 if ('serviceWorker' in navigator) {
-	console.log('TEST');
+	console.log('TEST01');
   navigator.serviceWorker.register('/TEST_RD/sw.js', { scope: '/TEST_RD/' }).then(function(reg) {
 
     if(reg.installing) {
@@ -13,12 +13,9 @@ if ('serviceWorker' in navigator) {
     } else if(reg.active) {
       console.log('# Service worker active #');
 	  document.getElementById("myTitle").innerHTML = "Service worker active";
-    }
-	var curVer = reg.getVersion();
-	var v1 = reg.Version;
+    }	var v1 = reg.Version;
 	  var v2 = reg.fncVersion();
 	  console.log("v1 : " + v1 + " - v2 : "+ v2);
-	console.log("Version actuelle : v"+curVer);
 	document.getElementById("myTitle").innerHTML += curVer;
   }).catch(function(error) {
     // registration failed
