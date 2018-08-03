3345c769e5485ca7aa5298802300891782698b9d
Ekorrigan <41920003+Ekorrigan@users.noreply.github.com>
2018-08-03 14:25:48 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 var Test=0;
-var Version='1.56';
+var Version='1.57';
 var forceNet = false;
 self.addEventListener('install', function(event) {
   console.log('Service Worker Version #' + Version);
@@ -85,7 +85,7 @@ self.addEventListener("message", event => {
 	caches.keys().then(function(keyList) {
 		Promise.all(keyList.map(function(key) {
 			if (key!== Version) {
-				console.log("delete cache : "+key);
+				console.log("delete cache : "+key+ " (v"+Version+")");
 				return caches.delete(key);
 			}
 		})).then(()=>{
@@ -104,7 +104,7 @@ self.addEventListener("message", event => {
 				]).then(function(){ 
 						forceNet= false;
 						localStorage.setItem("curVer", Version);
-						console.log("fin activation"+ localStorage.getItem("curVer"));
+						console.log("fin activation #"+ localStorage.getItem("curVer"));
 				});
 			});
 		});
