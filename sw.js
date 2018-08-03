82cc3dfb42db8b734f2c6aef4e7cad05f4de66b6
Ekorrigan <41920003+Ekorrigan@users.noreply.github.com>
2018-08-03 15:56:40 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 var Test=0;
-var Version='1.68d';
+var Version='1.68e';
 var forceNet = false;
 self.addEventListener('install', function(event) {
   console.log('Service Worker Version #' + Version);
